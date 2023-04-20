a6c4a97e2ca33a9430750e80c22eed9e0ca624d5
unknown <frannpacucci@gmail.com>
2023-04-20 18:18:25 -0300

    primer proyecto

---
+++ app.js
@@ -0,0 +1,10 @@
+function mensaje() {
+
+    Swal.fire({
+        position: 'top-end',
+        icon: 'success',
+        title: 'Your work has been saved',
+        showConfirmButton: false,
+        timer: 1500
+      })
+  };
